ae17194d8e577d09ed0160359d0a1c1ad24a171e
truongkute <91588296+truongkute@users.noreply.github.com>
2023-02-12 20:47:06 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import Board from './Components/Board/Board';
 import {useSelector} from 'react-redux';
 import Heading from './Components/Heading/Heading';
 import { rootState } from './Components/interface';
+import RefreshButton from './Components/RefreshButton';
 
 function App() {
  const board = useSelector((state:rootState)=> state.board.board);
   return (
     <div className="App">
     <Heading type="h1" text="Wordiee"/>
-    <Heading type="subtitle" text="Another Wordle Clone"/>
+    <Heading type="subtitle" text="Sáng Tạo Trẻ 2022-2023"/>
     <div className="board-container">
       <Board board={board}/>
+      <RefreshButton/>
     </div>
     </div>
   );
